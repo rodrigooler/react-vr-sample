136bc4d94b1747e21c40c81d78b141209fe00f6a
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2017-06-15 19:34:13 -0300

    Refactor index.vr.js

--- index.vr.js
+++ index.vr.js
@@ -1,44 +1,34 @@
-import React from 'react';
-import { AppRegistry, asset, Pano, Text, View } from 'react-vr';
+import React, { PureComponent } from 'react';
+import { AppRegistry, asset, Pano, Text, View, StyleSheet } from 'react-vr';
 
-export default class ReactVRSample extends React.Component {
+export default class ReactVRSample extends PureComponent {
   render() {
     return (
       <View>
         <Pano source={asset('chess-world.jpg')} />
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.3,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{ translate: [0, 0, -3] }],
-          }}
-        >
+        <Text style={styles.text}>
           React VR Sample
         </Text>
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.3,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{ translate: [0, 0, -3] }],
-          }}
-        >
+        <Text style={styles.text}>
           by @rodrigooler
         </Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  text: {
+    backgroundColor: '#777879',
+    fontSize: 0.3,
+    fontWeight: '400',
+    layoutOrigin: [0.5, 0.5],
+    paddingLeft: 0.2,
+    paddingRight: 0.2,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    transform: [{ translate: [0, 0, -3] }],
+  },
+});
 
 AppRegistry.registerComponent('ReactVRSample', () => ReactVRSample);
